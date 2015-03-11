2e7620bf78abeb1ed8fe54658aa032073f483a1d
Ronen Babayoff <ronen@lavaina.com>
2015-03-11 16:14:12 -0400

    Update loglevel to 1.2.0_1

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.onUse(function (api) {
   api.versionsFrom('0.9.3');
 
   api.use('coffeescript', 'server');
-  api.use(['practicalmeteor:loglevel@1.1.0_2', 'practicalmeteor:chai@1.9.2_3'], 'server');
+  api.use(['practicalmeteor:loglevel@1.2.0_1', 'practicalmeteor:chai@1.9.2_3'], 'server');
 
   // Uncomment once we upgrade to loglevel v2, with package specific loggers
   // api.addFiles('src/lib/log.js');
@@ -27,7 +27,7 @@ Package.onTest(function (api) {
   api.use([
     'coffeescript',
     'practicalmeteor:google-translate',
-    'practicalmeteor:loglevel@1.1.0_2',
+    'practicalmeteor:loglevel@1.2.0_1',
     'practicalmeteor:chai',
     'tinytest'], 'server');
 
